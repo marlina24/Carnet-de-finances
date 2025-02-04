daf8b3a719f75cb5617f35b8d8d499e47fd21793
Jhon Lloyd Omblero <113282952+joxyle-jhon@users.noreply.github.com>
2025-02-04 13:08:18 +0800

    Update DatabaseConnection.java

--- src/personal_financial_management/DatabaseConnection.java
+++ src/personal_financial_management/DatabaseConnection.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package personal_financial_management;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-/**
- *
- * @author k
- */
+
 public class DatabaseConnection {
     private static final String DB_NAME ="personal_financial_management"; //mao ni akong geh pangalan sa database sa PC
     private static final String JDBC_URL ="jdbc:mysql://localhost:3306/"+DB_NAME;
